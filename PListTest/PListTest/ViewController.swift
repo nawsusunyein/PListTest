7b6c8fbf8189bdf7676ab0a6e2ce9d705a437472
Naw Su Su Nyein <nawsusunyein@gmail.com>
2020-07-05 22:30:16 +0630

    create new project

---
+++ PListTest/PListTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PListTest
+//
+//  Created by Naw Su Su Nyein on 7/5/20.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
