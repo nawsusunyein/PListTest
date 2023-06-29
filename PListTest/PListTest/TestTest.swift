90a29061ac577112024f032173e65b0ee25f497e
Naw Su Su Nyein <nawsusunyein@gmail.com>
2023-06-30 05:51:06 +0900

    create new file

---
+++ PListTest/PListTest/TestTest.swift
@@ -0,0 +1,15 @@
+//
+//  TestTest.swift
+//  PListTest
+//
+//  Created by Naw Su Su Nyein on 30/06/2023.
+//  Copyright © 2023 Naw Su Su Nyein. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func testFunc() {
+        print("hee hee")
+    }
+}
