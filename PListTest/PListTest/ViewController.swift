e05090344c5b3fa25210a6f748dae5bf76379a77
Naw Su Su Nyein <nawsusunyein@gmail.com>
2024-02-19 22:03:43 +0900

    get google app ID

--- PListTest/PListTest/ViewController.swift
+++ PListTest/PListTest/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        if let fruits = getFruitPList(withName: "Fruit"){
-            print(fruits)
-            print("Value : \(fruits.Org)")
-            print("Value : \(fruits.Ki)")
-        }
+//        if let fruits = getFruitPList(withName: "Fruit"){
+//            print(fruits)
+//            print("Value : \(fruits.Org)")
+//            print("Value : \(fruits.Ki)")
+//        }
         
         print("blah blah PR test")
-        self.createWebPropertyList()
-        
+       // self.createWebPropertyList()
+        getGoogleAppId()
         let test = SwiftToast(text: "This is a Toast")
         present(test, animated: true)
         
@@ -42,14 +42,24 @@ class ViewController: UIViewController {
         print("url : \(AppConfig.baseURL)")
     }
 
-    func getFruitPList(withName name : String) -> Fruits?{
-        if let path = Bundle.main.path(forResource: name, ofType: "plist"),let xml = FileManager.default.contents(atPath: path),
-            let preferences = try? PropertyListDecoder().decode(Fruits.self, from: xml){
-           return preferences
+//    func getFruitPList(withName name : String) -> Fruits?{
+//        if let path = Bundle.main.path(forResource: name, ofType: "plist"),let xml = FileManager.default.contents(atPath: path),
+//            let preferences = try? PropertyListDecoder().decode(Fruits.self, from: xml){
+//           return preferences
+//        }
+//
+//    }
+    
+    func getGoogleAppId() {
+        if let path1 = Bundle.main.path(forResource: "GoogleService-Info-Dev", ofType: "plist"),let xml = FileManager.default.contents(atPath: path1)  {
+            print("xml:\(type(of: xml))")
+            let stringTest = String(decoding: xml, as: UTF8.self)
+            print("string value : \(stringTest)")
         }
         
-        return nil
     }
+        
+    
     
     func createWebPropertyList(){
         let webPropertyList = WebProperty(host:"https://www.google.com",path: "/your-name",port: "8080")
@@ -65,4 +75,39 @@ class ViewController: UIViewController {
         }
     }
 }
+
+
+
+class XMLParses : NSObject, XMLParserDelegate {
+ var dictionary = NSMutableDictionary()
+    var key : String?
+    var value : String?
+    
+    func parseXML(xmlData: Data) -> NSDictionary? {
+            let parser = XMLParser(data: xmlData)
+            parser.delegate = self
+            if parser.parse() {
+                return dictionary
+            } else {
+                print("Process Failed!")
+                return nil
+            }
+        }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            if let element = key, let value = value {
+                if let existingValue = dictionary[element] as? String {
+                    dictionary[element] = [existingValue, value]
+                }else if let existingValue = dictionary[element] as? [String] {
+                    dictionary[element] = existingValue + [value]
+                }else{
+                    dictionary[element] = value
+                }
+            }
+            
+            key = nil
+            value = nil
+        }
+}
+
 
